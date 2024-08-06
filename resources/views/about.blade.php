5216d1b44de1dcf9a64c5ad956493fd5b8e94350
Ariensya <222112044@stis.ac.id>
2024-08-06 14:38:54 +0700

    laravel update

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Monit.kel | about</title>
+</head>
+<body>
+    <h1>Halaman about</h1>
+    <h3>{{$name}}</h3>
+    <p><a href="">{{$email}}</a></p>
+    <img src="img/{{$img}}" alt="{{$name}}" width="200" >
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "BPS",
+        "email" => "bps@gmail.com",
+        "img" => "kantor-bps.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
+Route::get('/laravel', function () {
     return view('welcome');
 });
